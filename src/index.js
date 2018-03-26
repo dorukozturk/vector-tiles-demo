6c37b17a671c7fe9d52c31fa6821091305bc3387
Doruk Ozturk <doruk.ozturk@kitware.com>
2018-03-26 16:03:40 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import Map from 'ol/map';
+import View from 'ol/view';
+import VectorTileLayer from 'ol/layer/vectortile';
+import VectorTileSource from 'ol/source/vectortile';
+import MVT from 'ol/format/mvt';
+import Style from 'ol/style/style';
+import Fill from 'ol/style/fill';
+import Stroke from 'ol/style/stroke';
+import Circle from 'ol/style/circle';
+import Text from 'ol/style/text';
+import randomcolor from 'randomcolor';
+
+var layer = new VectorTileLayer({
+    declutter: true,
+    source: new VectorTileSource({
+	format: new MVT(),
+	url: 'http://192.168.30.198:9002/data/v3/{z}/{x}/{y}.pbf'
+    })
+})
+
+layer.setStyle(function(feature, resolution) {
+    // https://openlayers.org/workshop/en/vectortile/ugly.html
+    return new Style({
+      fill: new Fill({
+          color: randomcolor.randomColor()
+      })
+    });
+});
+
+var map = new Map({
+    layers: [layer],
+    target: 'map',
+    view: new View({
+        center: [0, 0],
+        zoom: 2
+    })
+});
